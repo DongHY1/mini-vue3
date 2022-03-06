4878b5b902c2db5f10fbf51322ec8488d412f157
donghy1 <915110101@qq.com>
2022-03-06 12:15:20 +0800

    实现computed happy path

---
+++ src/reactivity/__test__/computed.spec.ts
@@ -0,0 +1,16 @@
+import { reactive } from "../reactive"
+import { computed } from "../computed"
+describe("computed", () => {
+    it('happy path', () => {
+        const user = reactive({
+            age: 1
+        })
+        const age = computed(() => {
+            return user.age
+        })
+        expect(age.value).toBe(1)
+    })
+    it.skip('cache', () => {
+        //缓存功能
+    })
+})
